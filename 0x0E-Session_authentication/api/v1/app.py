13c10e53711c4baff7ab5edb330b19a21be25138
Solienne <soliene.teixeira@ufba.br>
2022-09-13 15:07:19 +0000

    Session

--- 0x0E-Session_authentication/api/v1/app.py
+++ 0x0E-Session_authentication/api/v1/app.py
@@ -13,12 +13,66 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+
+if getenv("AUTH_TYPE") == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+if getenv("AUTH_TYPE") == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+if getenv("AUTH_TYPE") == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
+if getenv("AUTH_TYPE") == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+
+if getenv("AUTH_TYPE") == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Error handler: Unauthorized
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Error handler: Forbidden
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """ Before_request handler
+    """
+    excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/']
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None and auth.session_cookie(
+                request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
